123e79538c571a75a6384e6b37a024d9735755ad
Rafael-Takematsu <rafatakematsu@gmail.com>
2026-08-28 11:39:51 -0300

    feat(dto): construindo classes do dto

---
+++ src/main/java/com/picpay/rh/dto/FuncionarioResponseDTO.java
@@ -0,0 +1,33 @@
+package com.picpay.rh.dto;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter @AllArgsConstructor
+public class FuncionarioResponseDTO implements Serializable{
+
+    public FuncionarioResponseDTO() {}
+
+
+    private int id;
+
+    private String nome;
+
+    private String email;
+
+    private String telefone;
+
+    private String cargo;
+
+    private String departamento;
+
+    private double salario;
+
+    private String cidade;
+
+    private String status;
+    
+}
